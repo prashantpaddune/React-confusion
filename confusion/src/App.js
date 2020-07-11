b49e13870de12f50405ec6b0549f923c47e2661c
prashantpaddune <elonpras@my.smccd.edu>
2020-07-11 15:25:34 +0530

    React Router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
-
+import { BrowserRouter as Router } from "react-router-dom";
+  
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <Main/>
-      </div>
+        <Router>
+          <div>
+            <Main/>
+          </div>
+        </Router>
     );
   }
 }
